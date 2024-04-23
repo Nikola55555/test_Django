2d7fb515205e216e3bc63d06377158f8b232383c
Nikola55555 <8637868@mail.ru>
2024-04-23 21:25:34 +0300

    1

---
+++ site_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    contact_info = models.TextField(max_length=200)
+    experience = models.IntegerField()
+
+
+class Executor(models.Model):
+    name = models.CharField(max_length=100)
+    contact_info = models.TextField(max_length=200)
+    experience = models.IntegerField()
